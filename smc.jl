caa423d7cb539c8397a78a9381b0a6dff23e8486
Nathan Cunningham <nathancunn@gmail.com>
2018-02-13 14:20:52 +0000

    Enable conditioning on data chunks

--- smc.jl
+++ smc.jl
@@ -5,66 +5,79 @@ using StaticArrays
 
 include("particles.jl")
 
-function pMDImodel(data, dataTypes, N, particles, nThreads, fullOutput, essThreshold)
+function pMDImodel(data, dataTypes, N, particles, s, ρ, nThreads, fullOutput, essThreshold)
     K       = length(data) # No. of datasets
     n_obs   = size(data[1])[1]
     d       = [size(data[k])[2] for k = 1:K]
     @assert length(dataTypes) == K
     @assert all(x->x==n_obs, [size(data[k])[1] for k = 1:K])
-
     # The mutation function
-    @inline function M!(newParticle::mdiParticle{K}, rng::SMCRNG, p::Int64,
+
+    function M!(newParticle::mdiParticle{K}, rng::SMCRNG, p::Int64,
       particle::mdiParticle{K}, scratch::Void) where {K}
+      i_1 = Int64(floor(ρ * n_obs))
+      K = length(data)
+      n_obs   = size(data[1])[1]
+
       if p == 1
-        for k = 1:K
-            # Set the particle to a container for the corresponding data type
-            newParticle.clusters[k] = dataTypes[k]{n_obs, d[k], N}()
-            # Add the first observation to cluster 1
-            particle_add(data[k][1, :], 1, 1, newParticle.clusters[k])
-        end
+      # Move the fixed observations to their cluster
+      for k = 1:K
+          newParticle.clusters[k] = dataTypes[k]{n_obs, d[k], N}()
+          for i = 1:(i_1 + 1)
+              i = Int64(i)
+              particle_add(data[k][i, :], Int64(s[k][i]), i, newParticle.clusters[k])
+          end
+      end
+      # Then add this first free observation to a cluster
+      logprob           = [calc_logprob(data[k][1 + i_1, :], newParticle.clusters[k]) for k = 1:K]
+      fprob             = [cumsum(exp.(logprob[k] .- maximum(logprob[k]))) for k = 1:K]
+      newParticle.logW  = newParticle.logW + sum(maximum.(logprob)) + sum(log.(last.(fprob)))
+      fprob             = fprob ./ last.(fprob)
+      sstar             = [find(fprob[k] .> rand())[1] for k = 1:K]
+
+      for k = 1:K
+          particle_add(data[k][1 + i_1, :], sstar[k], 1 + i_1, newParticle.clusters[k])
+      end
       else
-          #newParticle = particle
+          newParticle = particle
           if smcio.resample[p - 1] == true
               # Reset particle weights
               newParticle.logW = 0
               # Reallocate previous observations
               for j = 1:(p - 1)
-                  sstar             = [newParticle.clusters[k].c[j] for k = 1:K]
-                  #print(newParticle.clusters[1].c[1:p], "\n")
+                  sstar             = [newParticle.clusters[k].c[j + i_1] for k = 1:K]
                   for k = 1:K
-                      particle_remove(data[k][j, :], sstar[k], j, newParticle.clusters[k])
+                      particle_remove(data[k][j + i_1, :], sstar[k], j + i_1, newParticle.clusters[k])
                   end
-                  #print([newParticle.clusters[k].n for k = 1:K], "\n")
-                  logprob           = [calc_logprob(data[k][j, :], newParticle.clusters[k]) for k = 1:K]
+                  logprob           = [calc_logprob(data[k][j + i_1, :], newParticle.clusters[k]) for k = 1:K]
                   fprob             = [cumsum(exp.(logprob[k] .- maximum(logprob[k]))) for k = 1:K]
                   fprob             = fprob ./ last.(fprob)
                   sstar             = [find(fprob[k] .> rand())[1] for k = 1:K]
                   for k = 1:K
-                      particle_add(data[k][j, :], sstar[k], j, newParticle.clusters[k])
+                      particle_add(data[k][j + i_1, :], sstar[k], j + i_1, newParticle.clusters[k])
                   end
               end
         end
-          logprob           = [calc_logprob(data[k][p, :], newParticle.clusters[k]) for k = 1:K]
+          logprob           = [calc_logprob(data[k][p + i_1, :], newParticle.clusters[k]) for k = 1:K]
           fprob             = [cumsum(exp.(logprob[k] .- maximum(logprob[k]))) for k = 1:K]
           newParticle.logW  = newParticle.logW + sum(maximum.(logprob)) + sum(log.(last.(fprob)))
           fprob             = fprob ./ last.(fprob)
           sstar             = [find(fprob[k] .> rand())[1] for k = 1:K]
 
           for k = 1:K
-              particle_add(data[k][p, :], sstar[k], p, newParticle.clusters[k])
+              particle_add(data[k][p + i_1, :], sstar[k], p + i_1, newParticle.clusters[k])
           end
-         # print(newParticle.clusters[1].c[1:p], "next", "\n")
 
       end
     end
 
     # The particle weight
-    @inline function lG(p::Int64, particle::mdiParticle{K}, ::Void) where{K}
+    function lG(p::Int64, particle::mdiParticle{K}, ::Void) where {K}
       return particle.logW
     end
 
-    model = SMCModel(M!, lG, n_obs, mdiParticle{K}, Void)
-    smcio = smcio = SMCIO{model.particle, model.pScratch}(particles, n_obs, nThreads, fullOutput, essThreshold)
+    model = SMCModel(M!, lG, Int64(n_obs - floor(ρ * n_obs)), mdiParticle{K}, Void)
+    smcio = smcio = SMCIO{model.particle, model.pScratch}(particles,Int64(n_obs - floor(ρ * n_obs)), nThreads, fullOutput, essThreshold)
     return model, smcio
 
 end
--- test.jl
+++ test.jl
@@ -5,10 +5,16 @@ K = 2
 d     = [rand(2:5) for k = 1:K]
 dataTypes = [gaussianCluster for k = 1:K]
 data      = [[randn(Int64(n_obs * 0.5), d[k]) - 3; randn(Int64(n_obs * 0.5), d[k]) + 3] for k = 1:K]
+s = [repeat(1:2, inner = Int(n_obs * 0.5)) for k = 1:K]
+model, smcio = pMDImodel(data, dataTypes, 3, 32, s, 0.25, 1, false, 0.5)
+@time smc!(model, smcio)
 
 
+# Using the iris data
 using RDatasets
+K = 1
 data = [Matrix(dataset("datasets", "iris")[:, 1:4])]
-
-model, smcio = pMDImodel(data, dataTypes, 3, 10, 1, false, 0.5)
+dataTypes = [gaussianCluster]
+s = [repeat(1:3,  inner = 50)]
+model, smcio = pMDImodel(data, dataTypes, 3, 32, s, 0.25, 1, false, 0.5)
 @time smc!(model, smcio)
