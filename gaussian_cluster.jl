5ff95fc3eed9ee570c68f3d180e0134e7eee5335
Nathan Cunningham <nathancunn@gmail.com>
2018-05-01 17:38:46 +0100

    Remove some old comments and adapt to minimise memory allocations

--- gaussian_cluster.jl
+++ gaussian_cluster.jl
@@ -3,34 +3,32 @@
 ## d is dimension of data
 ## n_obs is the number of observations
 mutable struct gaussianCluster{n_obs, d, N}
-  # n is no. of clusters, d is dimension of data
-  # c::Int64 ## The cluster index
   # n::SVector{N, Int64}        ## number of obs in a cluster
-  n::Vector{Int64}
   # μ::MMatrix{N, d, Float64}   ## mean of observations in clusters
   # Σ::MMatrix{N, d, Float64}   ## sum of observations in clusters
   # λ::MMatrix{N, d,  Float64}
   # β::MMatrix{N, d, Float64}
+  # c::SVector{n_obs, Int64}    ## The allocation vector
+  n::Vector{Int64}
   μ::Matrix{Float64}   ## mean of observations in clusters
   Σ::Matrix{Float64}   ## sum of observations in clusters
   λ::Matrix{Float64}
   β::Matrix{Float64}
-  # c::SVector{n_obs, Int64}    ## The allocation vector
   c::Vector{Int64}    ## The allocation vector
-  ζ::Vector{Float64}        ## logprob
+  ζ::Vector{Float64}  ## logprob
   gaussianCluster{n_obs, d, N}() where {n_obs, d, N} = new(
                                               # Int64(1),
                                               # SVector{N, Int64}(zeros(Int64, N)),
-                                              Vector{Int64}(zeros(Int64, N)),
                                               # MMatrix{N, d, Float64}(zeros(Float64, N, d)),
                                               # MMatrix{N, d, Float64}(zeros(Float64, N, d)),
                                               # MMatrix{N, d, Float64}(ones(Float64, N, d)),
                                               # MMatrix{N, d, Float64}(ones(Float64, N, d)),
-                                              Matrix{Float64}(zeros(Float64, N, d)),
-                                              Matrix{Float64}(zeros(Float64, N, d)),
-                                              Matrix{Float64}(ones(Float64, N, d)),
-                                              Matrix{Float64}(ones(Float64, N, d)),
                                               # SVector{n_obs, Int64}(zeros(Int64, n_obs)),
+                                              Vector{Int64}(zeros(Int64, N)),
+                                              Matrix{Float64}(zeros(Float64, N, d)),
+                                              Matrix{Float64}(zeros(Float64, N, d)),
+                                              Matrix{Float64}(ones(Float64, N, d)),
+                                              Matrix{Float64}(ones(Float64, N, d)),
                                               Vector{Int64}(zeros(Int64, n_obs)),
                                               Vector{Float64}(zeros(Float64, N)))
 end
@@ -41,50 +39,47 @@ function calc_logprob!(obs::Vector{Float64}, cl::gaussianCluster{n_obs, d, N}) w
   #                    0.75 * log.(1 + 2 * (obs) .^ 2), 1)[1]
   cl.ζ .= sum(-1.310533 - 0.75 * log.(1.0 + 2 * obs .^ 2))
   # Identify all non-empty clusters
-  ind = SVector{N, Bool}(cl.n .!= 0)
-  cl.ζ[ind] = sum(lgamma.((cl.n[ind] + 2.0) / 2.0) .-
-                      lgamma.((1.0 + cl.n[ind]) / 2.0) .+
-                      0.5 .* log.(cl.λ[ind, :] ./ ((0.5 + 0.5 .* cl.n[ind]) .* π)) -
-                      ((cl.n[ind] + 2.0) ./ 2.0) .* log.(1.0 + (1.0 ./ (0.5 + cl.n[ind] .* 0.5)) .*
-                      (-cl.μ[ind, :] .+ transpose(obs)) .^ 2.0 .* cl.λ[ind, :])
+  ind = Vector{Bool}(cl.n .!= 0)
+  n = cl.n[ind] .* 0.5 .+ 0.5
+  λ = cl.λ[ind, :]
+
+  # Posterior predictive taking normal-gamma prior
+  cl.ζ[ind] = sum(lgamma.(n + 0.5) .-
+                      lgamma.(n) .+
+                      0.5 .* log.(λ ./ n) .-
+                      (n .+ 0.5) .* log.((1.0 ./ n) .*
+                      (- cl.μ[ind, :] .+ transpose(obs)) .^ 2.0 .* λ .+ 1.0) .-
+                      0.5723649429247001 ## 0.5 * log(π)
                   , 2)
+    return
     return
 end
 
 
-function particle_add!(obs, i, sstar::Int64, cl1::gaussianCluster{n_obs, d, N}) where {n_obs, d, N}
-  # cl.c            = sstar
-  cl = deepcopy(cl1)
-  cl.β[sstar, :]  -= 0.5 .* (- 2.0 * cl.Σ[sstar, :] .* cl.μ[sstar, :] +
-                            cl.n[sstar] .* cl.μ[sstar, :] .^ 2) .+
-                    (cl.n[sstar] / (2.0 * (cl.n[sstar] + 1.0))) .* cl.μ[sstar, :] .^ 2
-  # cl.n            = setindex(cl.n, cl.n[sstar] + 1, sstar)
-  cl.n[sstar]     += Int64(1)
-  cl.Σ[sstar, :]  += obs
-  cl.μ[sstar, :]  = cl.Σ[sstar, :] ./ (cl.n[sstar] + 1.0)
-  cl.β[sstar, :]  += 0.5 * ((obs .^ 2) - 2.0 * cl.Σ[sstar, :] .* cl.μ[sstar, :] .+
-                      cl.n[sstar] .* cl.μ[sstar, :] .^ 2) .+
+function particle_add(obs, i, sstar::Int64, cl_old::gaussianCluster{n_obs, d, N}) where {n_obs, d, N}
+  cl = deepcopy(cl_old)
+    cl.β[sstar, :]  -= 0.5 .* (- 2.0 * cl.Σ[sstar, :] .* cl.μ[sstar, :] +
+                              cl.n[sstar] .* cl.μ[sstar, :] .^ 2) .+
                       (cl.n[sstar] / (2.0 * (cl.n[sstar] + 1.0))) .* cl.μ[sstar, :] .^ 2
-  cl.λ[sstar, :]    = ((0.5 * cl.n[sstar] + 0.5) .* (cl.n[sstar] + 1.0)) ./
-                    (cl.β[sstar, :] .* (cl.n[sstar] + 2.0))
-  # cl.c            = setindex(cl.c, sstar, i)
-  cl.c[i]         = Int64(sstar)
+    cl.n[sstar]     += Int64(1)
+    cl.Σ[sstar, :]  += obs
+    cl.μ[sstar, :]  = cl.Σ[sstar, :] ./ (cl.n[sstar] + 1.0)
+    cl.β[sstar, :]  += 0.5 * ((obs .^ 2) - 2.0 * cl.Σ[sstar, :] .* cl.μ[sstar, :] .+
+                        cl.n[sstar] .* cl.μ[sstar, :] .^ 2) .+
+                        (cl.n[sstar] / (2.0 * (cl.n[sstar] + 1.0))) .* cl.μ[sstar, :] .^ 2
+    cl.λ[sstar, :]    = ((0.5 * cl.n[sstar] + 0.5) .* (cl.n[sstar] + 1.0)) ./
+                      (cl.β[sstar, :] .* (cl.n[sstar] + 2.0))
+    cl.c[i]         = Int64(sstar)
   return cl
 end
 
 function particle_reset!(cl::gaussianCluster{n_obs, d, N}) where {n_obs, d, N}
-  # fill!(cl.β, 1)
-  # fill!(cl.λ, 1)
-  # fill!(cl.μ, 0)
-  # fill!(cl.Σ, 0)
   cl.β .= Float64(1)
   cl.λ .= Float64(1)
   cl.μ .= Float64(0)
   cl.Σ .= Float64(0)
   cl.n .= Int64(0)
   cl.c .= Int64(0)
-  # cl.n = SVector{N, Int64}(zeros(Int64, N))
-  # cl.c = SVector{n_obs, Int64}(zeros(Int64, n_obs))
   return
 end
 
@@ -114,4 +109,11 @@ function particle_remove(obs, sstar::Int64, p::Int64, cl::gaussianCluster{n_obs,
 
   end
   return
+end
+
+
+function gaussian_normalise!(dataFile::Array)
+  μ = mapslices(median, dataFile, 1)
+  σ = 0.5 .* μ - mapslices(x -> quantile(x, 0.05), dataFile, 1) + eps(Float64)
+  dataFile = (dataFile .- μ) ./ σ
 end
