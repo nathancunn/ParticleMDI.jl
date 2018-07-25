e210ae151748f012230060506b77314f980c040b
Nathan Cunningham <nathancunn@gmail.com>
2018-07-25 11:35:44 +0100

    Rename gaussian to Gaussian for style adherence

--- datatypes/gaussian_cluster.jl
+++ datatypes/gaussian_cluster.jl
@@ -3,7 +3,7 @@
 ## d is dimension of data
 ## n_obs is the number of observations
 ## n_levels is the number of levels, used in multinomial
-mutable struct gaussianCluster
+mutable struct GaussianCluster
   n::Vector{Int64}
   μ::Matrix{Float64}   ## mean of observations in clusters
   Σ::Matrix{Float64}   ## sum of observations in clusters
@@ -11,7 +11,7 @@ mutable struct gaussianCluster
   β::Matrix{Float64}
   c::Vector{Int64}    ## The allocation vector
   #ζ::Vector{Float64}  ## logprob
-  gaussianCluster(dataFile::Array, N::Int64) =  new(Vector{Int64}(zeros(Int64, N)),
+  GaussianCluster(dataFile::Array, N::Int64) =  new(Vector{Int64}(zeros(Int64, N)),
                                       Matrix{Float64}(zeros(Float64, N, size(dataFile, 2))),
                                       Matrix{Float64}(zeros(Float64, N, size(dataFile, 2))),
                                       Matrix{Float64}(ones(Float64, N, size(dataFile, 2))),
@@ -21,14 +21,14 @@ mutable struct gaussianCluster
 end
 
 
-function calc_logprob!(obs::Array{Float64}, cl::gaussianCluster, logprob)
+function calc_logprob!(logprob::SubArray, obs::Array, cl::particleMDI.GaussianCluster)
   logprob[1] = - 1.310533
-   for q in 1:length(obs)
-      logprob[1] -=  0.75 * Base.Math.JuliaLibm.log(1.0 + 2.0 * obs[q] ^ 2.0)
+   for obs_q in obs
+      logprob[1] -=  0.75 * Base.Math.JuliaLibm.log(1.0 + 2.0 * obs_q ^ 2.0)
   end
 
   @simd for i = 2:length(logprob)
-    logprob[i] = logprob[1]
+    @inbounds logprob[i] = logprob[1]
   end
 
   # Iterate over clusters
@@ -49,29 +49,29 @@ function calc_logprob!(obs::Array{Float64}, cl::gaussianCluster, logprob)
     return
 end
 
-function particle_add!(obs::Array{Float64}, i::Int64, sstar::Int64, cl::gaussianCluster)
-  @simd for q = 1:length(obs)
-      cl.β[sstar, q]  -= 0.5 * (- 2.0 * cl.Σ[sstar, q] * cl.μ[sstar, q] +
+function particle_add!(cl::particleMDI.GaussianCluster, obs::Array, i::Int64, sstar::Int64)
+  @inbounds @simd for q = 1:length(obs)
+    cl.β[sstar, q]  -= 0.5 * (- 2.0 * cl.Σ[sstar, q] * cl.μ[sstar, q] +
                              (cl.μ[sstar, q] ^ 2) * cl.n[sstar]) +
                     (cl.n[sstar] / (2.0 * (cl.n[sstar] + 1.0))) * cl.μ[sstar, q] ^ 2
   end
   @inbounds cl.n[sstar]     += Int64(1)
   @inbounds cl.Σ[sstar, :]  += obs
-  @simd for q = 1:length(obs)
-  @inbounds cl.μ[sstar, q]  = cl.Σ[sstar, q] / (cl.n[sstar] + 1.0)
-  cl.β[sstar, q]  += 0.5 * ((obs[q] ^ 2) -
+  @inbounds @simd for q = 1:length(obs)
+    cl.μ[sstar, q]  = cl.Σ[sstar, q] / (cl.n[sstar] + 1.0)
+    cl.β[sstar, q]  += 0.5 * ((obs[q] ^ 2) -
                       2.0 * cl.Σ[sstar, q] * cl.μ[sstar, q] +
                       cl.n[sstar] * cl.μ[sstar, q] ^ 2) +
                       (cl.n[sstar] / (2.0 * (cl.n[sstar] + 1.0))) *
                       cl.μ[sstar, q] ^ 2
-  @inbounds cl.λ[sstar, q]    = ((0.5 * cl.n[sstar] + 0.5) * (cl.n[sstar] + 1.0)) /
+    cl.λ[sstar, q]    = ((0.5 * cl.n[sstar] + 0.5) * (cl.n[sstar] + 1.0)) /
                     (cl.β[sstar, q] * (cl.n[sstar] + 2.0))
   end
-  @inbounds cl.c[i]         = Int64(sstar)
+  @inbounds cl.c[i]         = sstar
   return
 end
 
-function particle_reset!(cl::gaussianCluster)
+function particle_reset!(cl::particleMDI.GaussianCluster)
   cl.β .= Float64(1)
   cl.λ .= Float64(1)
   cl.μ .= Float64(0)
@@ -94,7 +94,7 @@ function gaussian_normalise!(dataFile::Array)
   return
 end
 
-function particle_copy(particle::gaussianCluster)
+function particle_copy(particle::particleMDI.GaussianCluster)
     # As the mutation step involves copy the contents of a particle
     # This is slightly more efficient than deepcopy
     out = typeof(particle)()
