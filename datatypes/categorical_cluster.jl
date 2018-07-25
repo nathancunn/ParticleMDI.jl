d03d7dad123a3e0ad5e448540c6531255b04c187
Nathan Cunningham <nathancunn@gmail.com>
2018-07-25 11:35:24 +0100

    Rename multinomial to categorical, fix logprob calculation

--- datatypes/categorical_cluster.jl
+++ datatypes/categorical_cluster.jl
@@ -1,20 +1,20 @@
-## Define a particle consisting of all cluster variables for a multinomial mixture
-mutable struct multinomialCluster
+## Define a particle consisting of all cluster variables for a Categorical mixture
+mutable struct CategoricalCluster
   n::Vector{Int64}        # count of cluster members
   counts::Vector{Array{Int64}}    # count of occurrence of levels
   c::Vector{Int64}        # The allocation vector
   # ζ::Vector{Float64}      # logprob
-  multinomialCluster(dataFile, N) = new(Vector{Int64}(zeros(Int64, N)),
+  CategoricalCluster(dataFile, N) = new(Vector{Int64}(zeros(Int64, N)),
                                         [zeros(Int64, maximum(dataFile), N) for d = 1:size(dataFile, 2)],
                                         Vector{Int64}(zeros(Int64, size(dataFile, 1))))
                                         # , Vector{Float64}(zeros(Float64, N)))
 end
 
-function calc_logprob!(obs::Array{Int64}, cl::multinomialCluster, logprob)
+function calc_logprob!(logprob, obs::Array{Int64}, cl::CategoricalCluster)
   Q = length(obs)
   for n in 1:length(logprob)
       # @inbounds cl.ζ[n] = - log(cl.n[n] + 0.5 * size(cl.counts[1], 2)) * length(obs)
-      @inbounds logprob[n] = - Base.Math.JuliaLibm.log(cl.n[n] + 0.5 * size(cl.counts[1], 2)) * Q
+      @inbounds logprob[n] = - Base.Math.JuliaLibm.log(cl.n[n] + 0.5 * size(cl.counts[1], 1)) * Q
       if cl.n[n] == 0
           @inbounds logprob[n] += Base.Math.JuliaLibm.log(0.5) * Q
       else
@@ -26,7 +26,7 @@ function calc_logprob!(obs::Array{Int64}, cl::multinomialCluster, logprob)
   end
 end
 
-function particle_add!(obs::Array{Int64}, i::Int64, sstar::Int64, cl::multinomialCluster)
+function particle_add!(cl::CategoricalCluster, obs::Array{Int64}, i::Int64, sstar::Int64)
   @inbounds cl.n[sstar]     += Int64(1)
   @simd for q = 1:length(obs)
     @inbounds cl.counts[q][obs[q], sstar] += Int64(1)
@@ -35,7 +35,7 @@ function particle_add!(obs::Array{Int64}, i::Int64, sstar::Int64, cl::multinomia
   return
 end
 
-function particle_reset!(cl::multinomialCluster)
+function particle_reset!(cl::CategoricalCluster)
   for k = 1:length(cl.counts)
     cl.counts[k] .= Int64(0)
   end
