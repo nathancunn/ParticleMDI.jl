37d09593f8249177861b8b77f98c4a3b452a7f59
nathancunn <nathancunn@gmail.com>
2018-09-27 10:42:21 +0100

    Remove superceded functions

--- src/misc.jl
+++ src/misc.jl
@@ -12,75 +12,6 @@ function calculate_Φ_lab(K::Int64)
     return Φ_lab
 end
 
-
-function draw_sstar!(sstar::Array, logprob, particle::Array, particle_IDs::Array, Π::Array, K::Int64, N::Int64, ancestor_weights::Vector, logweight::Vector, s)
-    fprob = Vector{Float64}(N)
-    for k = 1:K
-        for p = 1:maximum(particle_IDs[:, k])
-            @inbounds particle_flag = findindices(particle_IDs[:, k], p)
-            @inbounds max_p = maximum(logprob[:, p, k])
-            for n = 1:N
-                @fastmath @inbounds fprob[n] = Π[n, k] * exp(logprob[n, p, k] - max_p)
-            end
-            # Draw sstar
-            # @inbounds @fastmath sstar[particle_flag, k] = sample(Vector{Int64}(1:N), Weights(fprob), length(particle_flag))
-            @inbounds sstar[particle_flag, k] = sampleCategorical(length(particle_flag), fprob)
-            # Update ancestor weights
-            for p_flag in particle_flag
-                @inbounds @fastmath ancestor_weights[p_flag] += log(fprob[s[k]] + eps(Float64))
-                # Update logweight
-                @inbounds @fastmath logweight[p_flag] += log(sum(fprob)) + max_p
-            end
-        end
-    end
-    return
-end
-
-function draw_sstar_p!(sstar, logprob, Π::Array, N::Int64, ancestor_weights::Vector, logweight, s)
-    fprob = Vector{Float64}(N)
-    # @inbounds particle_flag = findindices(particle_IDs, p)
-    @inbounds max_p = maximum(logprob)
-    for n = 1:N
-        @fastmath @inbounds fprob[n] = Π[n] * exp(logprob[n] - max_p)
-    end
-    # Draw sstar
-    @inbounds sstar = sampleCategorical(length(sstar), fprob)
-    # Update ancestor weights
-    #for p_flag in particle_flag
-    @inbounds @fastmath ancestor_weights += log(fprob[s] + eps(Float64))
-        # Update logweight
-        @inbounds @fastmath logweight .+= log(sum(fprob)) + max_p
-    #end
-    return (sstar, logweight)
-end
-
-@inline function update_particleIDs(particle_IDs, sstar, particles, N)
-    return ID_to_canonical!(particle_IDs * (particles * N) + sstar)
-end
-
-@inline function update_particleIDs!(particle_IDs, sstar, particles, N)
-    for k in 1:size(particle_IDs, 2)
-        for p in 1:size(particle_IDs, 1)
-            particle_IDs[p, k] = particle_IDs[p, k] * (particles * N) + sstar[p, k]
-        end
-    end
-    ID_to_canonical!(particle_IDs)
-end
-
-function ID_to_canonical!(x)
-    for (i, xi) in enumerate(x)
-        if xi > 0
-            for k in eachindex(x)
-                if x[k] == xi
-                    x[k] = - i
-                end
-            end
-        end
-    end
-    x = - x
-end
-
-
 @inline function calc_ESS(logweight)
     ESSnum = 0.0
     ESSdenom = 0.0
@@ -106,7 +37,13 @@ function draw_partstar(logweight, particles)
             partstar[i] = p
         end
     end
+    # Systematic resampling not ideal for CSMC
+    # Particle indices are sorted, so can't just set
+    # the first as reference trajectory
+    # Instead: shuffle, replace first, sort
+    shuffle!(partstar)
     partstar[1] = 1
+    sort!(partstar)
     return partstar
 end
 
@@ -125,14 +62,6 @@ function Φ_upweight!(logweight, sstar, K::Int64, Φ, particles)
     end
     return
 end
-
-
-function Φ_involvement(K::Int64, k::Int64)
-    Φ_lab = calculate_Φ_lab(K)
-    Φ_involved = (Φ_lab[:, 1] .== k) .| (Φ_lab[:, 2] .== k)
-    return Φ_involved
-end
-
 
 function align_labels!(s::Array, Φ::Array, γ::Array, N::Int64, K::Int64)
     if K == 1
@@ -174,18 +103,6 @@ function align_labels!(s::Array, Φ::Array, γ::Array, N::Int64, K::Int64)
     end
 end
 
-
-function ID_match(particle_IDs, new_particle_IDs, particles::Int64)
-    # Find the minimum new_particle_ID corresponding
-    # to a particular particle_ID
-    out = Array{Int64}(particles)
-    for u in unique(particle_IDs)
-        out[u] = minimum(new_particle_IDs[findindices(particle_IDs, u)])
-    end
-    return out
-end
-
-
 function findindex(A, b)
     # Find first occurrence of b in A
     for (i,a) in enumerate(A)
@@ -207,6 +124,20 @@ function findindices(A, b)
     return out
 end
 
+function findZindices(k, K, n, N)
+    # Instead of searching where the γc_combn = a
+    # by searching the vector
+    # We can know this in advance
+    # This way is >10x faster for larger problems
+    out = zeros(Bool, N ^ K)
+    start = (n - 1) * N ^ (k - 1) + 1
+    for i in 1:(N ^ (K - k))
+        out[start:(start - 1 + N ^ (k - 1))] .= true
+        start += N ^ k
+    end
+    return out
+end
+
 function countn(A, b)
     out = 0
     for a in A
@@ -216,21 +147,6 @@ function countn(A, b)
     end
     return out
 end
-
-
-
-function findgammas(n::Int64, k::Int64, N::Int64, K::Int64)
-    pertinent_rows = Vector{Int64}(N ^ (K - 1))
-    count = 1
-         for j in collect(1:(N ^ (k - 1)))
-             for i in collect(0:(N ^ k):(N ^ K - 1))
-             pertinent_rows[count] = Int64(i + j + n - 1)
-             count += 1
-         end
-    end
-    return pertinent_rows
-end
-
 
 function wipedout(v1, v2, x)
     count1 = 0
