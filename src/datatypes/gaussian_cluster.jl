d0f2441927905193c8a4cc66b7ec76b97dace686
nathancunn <nathancunn@gmail.com>
2018-10-30 15:14:06 +0000

    Update Gaussian data type

--- src/datatypes/gaussian_cluster.jl
+++ src/datatypes/gaussian_cluster.jl
@@ -20,34 +20,55 @@ end
 
 function calc_logprob(obs::Array, cl::GaussianCluster)
   # Iterate over clusters
-  n = cl.n * 0.5 + 0.5
+   """
+   n = cl.n * 0.5 + 0.5
   out = length(obs) * (SpecialFunctions.lgamma((n) + 0.5) -
                                                 SpecialFunctions.lgamma(n) -
                                                 0.5723649429247001)
   # Iterate over features
-    for q in 1:length(obs)
-    @inbounds    out +=  0.5 * log(cl.λ[q] / n) -
-                            (n + 0.5) * log((1.0 / n) *
-                            (obs[q] - cl.μ[q]) ^ 2.0 * cl.λ[q] + 1.0)
+    for q in 1:size(obs, 1)
+      @inbounds out += 0.5 * log(cl.λ[q] / n) -
+                      (n + 0.5) * log((1.0 / n) *
+                      (obs[q] - cl.μ[q]) ^ 2.0 * cl.λ[q] + 1.0)
     end
-
     return out
+  """
+
+    out = length(obs) * (- 0.5723649429247001 + # log(1 / sqrt(pi))
+                          SpecialFunctions.lgamma(0.5 * cl.n + 1.0) -
+                          SpecialFunctions.lgamma(0.5 * cl.n + 0.5))
+    # Iterate over features
+    # Need to fix this :eyeroll:
+      for q in 1:size(obs, 1)
+        @inbounds out += 0.5 * (log(cl.λ[q]) - log(cl.n + 1.0)) -
+                        (0.5 * cl.n + 1.0) *
+                        log(1.0 + (1.0 / (cl.n + 1.0)) *
+                        ((obs[q] - cl.μ[q]) ^ 2.0) * cl.λ[q])
+      end
+      return out
+  # """
 end
+
 function cluster_add!(cl::GaussianCluster, obs::Array)
   @inbounds for q = 1:length(obs)
+    """
     cl.β[q]  -= 0.5 * (- 2.0 * cl.Σ[q] * cl.μ[q] +
                              (cl.μ[q] ^ 2) * cl.n) +
                     (cl.n / (2.0 * (cl.n + 1.0))) * cl.μ[q] ^ 2
+    """
   end
   @inbounds cl.n     += 1
   @inbounds for q = 1:length(obs)
     cl.Σ[q]  += obs[q]
+    cl.β[q] += cl.n * (obs[q] - cl.μ[q]) ^ 2 / (2 * (cl.n + 1))
     cl.μ[q]  = cl.Σ[q] / (cl.n + 1.0)
+    """
     cl.β[q]  += 0.5 * ((obs[q] ^ 2) -
                       2.0 * cl.Σ[q] * cl.μ[q] +
                       cl.n * cl.μ[q] ^ 2) +
                       (cl.n / (2.0 * (cl.n + 1.0))) *
                       cl.μ[q] ^ 2
+    """
     cl.λ[q]    = ((0.5 * cl.n + 0.5) * (cl.n + 1.0)) /
                     (cl.β[q] * (cl.n + 2.0))
   end
